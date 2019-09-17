04de4b446c416c724844cb397887fdd14a61d466
Adhi Wibawa <adhi@kora.id>
2019-09-17 18:42:07 +0700

    Update MainActivity

--- app/src/main/java/com/midi/endlessservice/activities/MainActivity.kt
+++ app/src/main/java/com/midi/endlessservice/activities/MainActivity.kt
@@ -1,40 +1,55 @@
 package com.midi.endlessservice.activities
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import com.midi.endlessservice.R
+import com.midi.endlessservice.utils.Actions
+import com.midi.endlessservice.utils.EndlessService
+import com.midi.endlessservice.utils.ServiceState
+import com.midi.endlessservice.utils.getServiceState
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "ENDLESS_SERVICE"
+
+    private var isStartService: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener {
+            toggleStart()
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    private fun toggleStart(){
+        if (!isStartService){
+            isStartService = true
+            actionOnService(Actions.START)
+        } else {
+            isStartService = false
+            actionOnService(Actions.STOP)
+        }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    private fun actionOnService(action: Actions) {
+        if (getServiceState(this) == ServiceState.STOPPED && action == Actions.STOP) return
+        Intent(this, EndlessService::class.java).also {
+            it.action = action.name
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                Log.d(TAG, "Starting the service in >=26 Mode")
+                startForegroundService(it)
+                return
+            }
+            Log.d(TAG, "Starting the service in < 26 Mode")
+            startService(it)
         }
     }
 }
